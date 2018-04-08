8967444438b5e23351078dfaeb1ad051be137b98
kwiecien <k.d.kwiecinski@gmail.com>
2018-04-08 22:15:09 +0200

    Detect faces

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,30 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+public class Emojifier {
+
+    private static final String LOG_TAG = Emojifier.class.getSimpleName();
+
+    public static int detectFaces(Bitmap bitmap, Context context) {
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+        Frame frame = new Frame.Builder()
+                .setBitmap(bitmap)
+                .build();
+        SparseArray<Face> faces = detector.detect(frame);
+        Log.d(LOG_TAG, "detectFaces: number of faces = " + faces.size());
+        detector.release();
+        return faces.size();
+    }
+
+}
